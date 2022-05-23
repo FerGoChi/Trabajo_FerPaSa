4478110189bd19f5975bc067633611fcdd86b48a
ALUMNOS <ALUMNOS@P503>
2022-05-23 09:17:51 +0200

    a

--- src/Calculadora/CalculadoraMain.java
+++ src/Calculadora/CalculadoraMain.java
@@ -1,92 +1,183 @@
 package Calculadora;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
+
+
 
 public class CalculadoraMain {		
 
-			
-				public static void main(String[] args) {
-
-					Scanner ent = new Scanner(System.in);
-					int opcion = 0;
-					float op1 = 0.0f, op2 = 0.0f;
-
-					do {
-					System.out.println("Selecciona una opcion \n" +
-
-					"1Âº)suma \n" +
-					"2Âº)resta \n" +
-					"3Âº)multiplicacion \n" +
-					"4Âº)division \n" +
-					"5Âº)Operacion simple\n" +
-					"6Âº)salir \n");
-
-					opcion = ent.nextInt();
-
-
-					if (opcion >=1 && opcion  <=4) {
+	public static void main(String[] args) {
+				
+		Scanner ent = new Scanner(System.in);
+		int opcion = 0;
+		float op1 = 0.0f, op2 = 0.0f;
+				
+		do {
+			System.out.println("Selecciona una opcion \n" +
+							
+				"1º)suma \n" +
+				"2º)resta \n" +
+				"3º)multiplicacion \n" +
+				"4º)division \n" +
+				"5º)Operacion simple\n" +
+				"6º)salir \n");
+				
+				opcion = ent.nextInt();
+					
+				
+			if (opcion >=1 && opcion  <=4) {
 					System.out.println("Introduce un numero");
 					op1 = ent.nextFloat();
 					System.out.println("Introduce un otro numero");
 					op2 = ent.nextFloat();
-					}
+			}
+				
+			switch (opcion) {
+			
+				case 1:
+					System.out.println(op1 + " + " + op2 + " = "+ (op1+op2));
+					break;
+				case 2:
+					System.out.println(op1 + " - " + op2 + " = "+ (op1-op2));
+					break;
+				case 3:
+					System.out.println(op1 + " x " + op2 + " = "+ (op1*op2));
+					break;
+				case 4:
+					if (op2 !=0)	{	
+						System.out.println(op1 + " / " + op2 + " = "+ (op1/op2));
+						
+					}else { 
+						System.out.println("la division por 0 no es posible");
+					 }
+					break;
+				case 5: 
 					
-					switch (opcion) {
-
-					case 1:
-						sumar(op1,op2);
-					break;
-					case 2:
-						restar(op1,op2);
-					break;
-					case 3:
-						multiplicar(op1,op2);
-					break;
-					case 4:
-						dividir(op1,op2);
-					break;
-					case 5:
-
-					System.out.println("Introduce una operaciÃ³n simple, sin numeros negativos, letras o parÃ©ntesis");
+					System.out.println("Introduce una operación simple, sin numeros negativos, letras o paréntesis");
 					ent.nextLine();
 					String operacion = ent.nextLine();
+					operacion = calcStr(operacion);
 					System.out.println(operacion);
-
+					
 					break;
-					case 6:
+				case 6:
 					System.out.println("adios");
 					break;
-					default:
+				default:
 					System.out.println("valor incorrecto");
+				
+			}
+				
+		} while (opcion !=6);			
+	}
+			
 
-					}
-
-					} while (opcion !=6);
-				}
-				public static void sumar (float o1, float o2) {
+	
+	
+	
+	
+			static public String calcStr(String operacion) {
+				try {	
+					float n1 = 0, n2 = 0;
+					int index=0, op = 0;
+					String split[] = null;
 					
-					System.out.println(o1 + " + " + o2 + " = "+ (o1+o2));
-							
-				}
+					if(operacion.contains("/") || operacion.contains("*")) {
+						split = operacion.split("\\+|\\-");
 						
-				public static void restar (float o1, float o2) {
-								
-					System.out.println(o1 + " + " + o2 + " = "+ (o1-o2));
-								
-				}
-				public static void multiplicar (float o1, float o2) {
+						for(index = 0 ; index<split.length; index++) {
 							
-					System.out.println(o1 + " + " + o2 + " = "+ (o1*o2));
-							
-				}
+							if(split[index].contains("/") || split[index].contains("*")) {
+								
+								String subSplit[] = split[index].split("\\*|\\/");
+								ArrayList<Character> subOperadores = new ArrayList<Character>();
+								getOperadores(split[index], subOperadores);
+								
+								n1 = Float.parseFloat(subSplit[0]);
+								op = 0;
+								n2 = 0;
+								
+								for(int r = 1 ; r<subSplit.length ; r++) {
+									
+									n2 = Float.parseFloat(subSplit[r]);
+									
+									switch (subOperadores.get(op)) {
+									
+									
+										case '+' -> n1 = (n1 + n2);
+										case '-' -> n1 = (n1 - n2);
+										case '*' -> n1 = (n1 * n2);
+										case '/' -> n1 = (n1 / n2);
+									}
+								
+									op++;
+								}
 						
-				public static void dividir (float o1, float o2) {
+								operacion = operacion.replaceFirst("\\Q"+split[index]+"\\E", String.valueOf(n1)); 
+								
+							}
+						}
+						
+						operacion = calcBajaPrioridad(operacion, split, n1, n2, op);
+					}
+					
+					else {
+						
+						operacion = calcBajaPrioridad(operacion, split, n1, n2, op);
+						
+					}
 							
-					if (o2 !=0) {		
-					System.out.println(o1 + " / " + o2 + " = "+ (o1/o2));
-					}else { 
-					System.out.println("la division por 0 no es posible");
+					operacion = "Reesultado de la operacion: "+operacion+"\n";
+					return operacion;
+					
+				}catch(NumberFormatException ex) {
+					return operacion = "Esta opción solo acepta operaciones simples (Suma, resta, multiplicación, División.\n"
+							+ "No se aceptan números negativos, parentesis o letras.\n";
+				}
+			}
+			
+			static public void getOperadores(String operacion, ArrayList<Character> operadores ){		
+				for(int i = 0; i<operacion.length(); i++) {
+		
+					if(operacion.charAt(i)=='*' || operacion.charAt(i)=='/' ||
+					   operacion.charAt(i)=='+' || operacion.charAt(i)=='-') {
+						
+						operadores.add(operacion.charAt(i));	
+						
 					}
 				}
-}
+			}
+			
+			static public String calcBajaPrioridad(String operacion, String split[], float n1, float n2, int op){
+				split = operacion.split("\\+|\\-");
+				ArrayList<Character> operadores = new ArrayList<Character>();
+				getOperadores(operacion, operadores);
+				
+				n1 = Float.parseFloat(split[0]);
+				op = 0;
+				n2 = 0;
+				
+				for (int i = 1; i < split.length ; i++) {
 
+					n2 = Float.parseFloat(split[i]);
+					
+					switch (operadores.get(op)) {
+					
+					
+						case '+' -> n1 = (n1 + n2);
+						case '-' -> n1 = (n1 - n2);
+						case '*' -> n1 = (n1 * n2);
+						case '/' -> n1 = (n1 / n2);
+					}
+					
+					op++;
+					
+				}
+				return operacion = ""+n1+"";
+			
+			}
+			
+
+}
